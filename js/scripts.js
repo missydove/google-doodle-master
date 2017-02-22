511e1f8fc56eac2e61793c0a2caa34758d3508f4
missydove <mmeltingtallow@vcd-ceb131-10.eastern.ewu.edu>
2017-02-22 09:33:27 -0800

    got some pics but aint working

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// YOUR SCRIPTS GO HERE
+
+var img0 = '../img/400.jpg';
+var img1 = '../img/600.jpg';
+var img2 = '../img/800.jpg';
+
+
+
+// Get Random Number
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+ = $('img').attr('src', 'img'+numRand) );
